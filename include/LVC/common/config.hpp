b91fc772f7ec4d90279e9292580ed7823e7874b5
Starry-OvO <starry.qvq@gmail.com>
2023-06-20 19:20:03 +0800

    fix: multiple definition with gcc

--- include/LVC/common/config.hpp
+++ include/LVC/common/config.hpp
@@ -60,7 +60,7 @@ public:
 
     class iterator;
 
-    static LVC_FORCE_INLINE MicroImageRanges fromConfig(const Config& cfg) noexcept;
+    static MicroImageRanges fromConfig(const Config& cfg) noexcept;
 
     iterator begin() const noexcept;
     iterator end() const noexcept;
@@ -101,69 +101,5 @@ private:
     bool is_horizontal_;
     bool is_right_shift_;
 };
-
-MicroImageRanges MicroImageRanges::fromConfig(const Config& cfg) noexcept
-{
-    int num_x, num_y;
-    if (cfg.getIsHorizontal()) {
-        num_x = calcNumHorizontal(cfg.getWidth(), cfg.getDiameter());
-        num_y = calcNumVertical(cfg.getHeight(), cfg.getDiameter());
-    } else {
-        num_x = calcNumVertical(cfg.getWidth(), cfg.getDiameter());
-        num_y = calcNumHorizontal(cfg.getHeight(), cfg.getDiameter());
-    }
-
-    return {cfg.getStartX(), cfg.getStartY(), cfg.getIntervalX(),    cfg.getIntervalY(),
-            num_x,           num_y,           cfg.getIsHorizontal(), cfg.getIsRightShift()};
-}
-
-LVC_FORCE_INLINE MicroImageRanges::iterator MicroImageRanges::begin() const noexcept
-{
-    return {start_x_, start_y_, interval_x_, interval_y_, 0, 0, num_x_, num_y_, is_horizontal_, is_right_shift_};
-}
-
-LVC_FORCE_INLINE MicroImageRanges::iterator MicroImageRanges::end() const noexcept
-{
-    int index_x = num_x_ * static_cast<int>(!is_horizontal_);
-    int index_y = num_y_ * static_cast<int>(is_horizontal_);
-    return {0.0, 0.0, interval_x_, interval_y_, index_x, index_y, num_x_, num_y_, is_horizontal_, is_right_shift_};
-}
-
-LVC_FORCE_INLINE MicroImageRanges::iterator& MicroImageRanges::iterator::operator++() noexcept
-{
-    if (is_horizontal_) {
-        index_x_ += 1;
-        x_ += interval_x_;
-        if (index_x_ == num_x_) {
-            index_x_ = 0;
-            index_y_ += 1;
-            y_ += interval_y_;
-            x_ -= interval_x_ * static_cast<float>(num_x_);
-            int is_odd = index_y_ & 1;
-            // | is_odd | is_right_shift | right or left |
-            // | ------ | -------------- | ------------- |
-            // | 1      | 1              | right         |
-            // | 1      | 0              | left          |
-            // | 0      | 1              | left          |
-            // | 0      | 0              | right         |
-            x_ += static_cast<float>(((is_odd + static_cast<int>(is_right_shift_)) & 1) * (-2) + 1) *
-                  (interval_x_ / 2.0f);
-        }
-    } else {
-        // just swap x and y
-        index_y_ += 1;
-        y_ += interval_y_;
-        if (index_y_ == num_y_) {
-            index_y_ = 0;
-            index_x_ += 1;
-            x_ += interval_x_;
-            y_ -= interval_y_ * static_cast<float>(num_y_);
-            int is_odd = index_x_ & 1;
-            y_ += static_cast<float>(((is_odd + static_cast<int>(is_right_shift_)) & 1) * (-2) + 1) *
-                  (interval_y_ / 2.0f);
-        }
-    }
-    return *this;
-}
 
 } // namespace lvc
