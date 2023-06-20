81edf90cdf85401c2e22187d196ccc6ca92b76d5
riverzhao <buaariverzhao@163.com>
2023-06-20 19:27:06 +0800

    process.cpp

--- apps/process.cpp
+++ apps/process.cpp
@@ -29,16 +29,24 @@ int main()
 
     char buf[256];
 
-    for (int i = 1; i < 31; i++) {
-        cv::Mat src, dst, backward_src;
-        snprintf(buf, 256, "D:\\Code\\SIGS\\230508_LVC\\code\\rlc-master\\TestDataset\\Tunnel_Train\\src\\%03d.png", i);
-        src = cv::imread(buf);
-        lvc::preprocessForward(cfg, src, dst);
-        lvc::preprocessBackward(cfg, dst, backward_src);
-        snprintf(buf, 256, "D:\\Code\\SIGS\\230508_LVC\\code\\rlc-master\\TestDataset\\Tunnel_Train\\src_pre\\%03d.png",
-                 i);
-        cv::imwrite(buf, backward_src);
-    }
+    // for (int i = 1; i < 31; i++) {
+    //     cv::Mat src, dst, backward_src;
+    //     snprintf(buf, 256, "D:\\Code\\SIGS\\230508_LVC\\code\\rlc-master\\TestDataset\\Tunnel_Train\\src\\%03d.png", i);  // snprintf 用于构建字符串
+    //     src = cv::imread(buf);
+    //     lvc::preprocessForward(cfg, src, dst);
+    //     lvc::preprocessBackward(cfg, dst, backward_src);
+    //     snprintf(buf, 256, "D:\\Code\\SIGS\\230508_LVC\\code\\rlc-master\\TestDataset\\Tunnel_Train\\src_pre\\%03d.png",
+    //              i);
+    //     cv::imwrite(buf, backward_src);
+    // }
+
+    cv::Mat src, dst, backward_src;
+    char inputPath[256] = "/mnt/e/Projects/task2-raytrix/libLVC/data/RawImages/NagoyaFujita.png";
+    char outputPath[256] = "/mnt/e/Projects/task2-raytrix/libLVC/data/PreprocessedImages/NagoyaFujita.png";
+    src = cv::imread(inputPath);
+    lvc::preprocessForward(cfg, src, dst);
+    lvc::preprocessBackward(cfg, dst, backward_src);
+    cv::imwrite(outputPath, backward_src);
 
     return 0;
 }
