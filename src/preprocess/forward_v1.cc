67636c4ff2c7382d8456c5e23fe3ba5785b563f2
Starry-OvO <starry.qvq@gmail.com>
2023-09-01 20:43:57 +0800

    fix: consistent naming

--- src/preprocess/forward_v1.cc
+++ src/preprocess/forward_v1.cc
@@ -8,17 +8,17 @@ namespace lvc {
 
 void preprocessForward(const Config& cfg, const cv::Mat& src, cv::Mat& dst)
 {
-    double inscribed_square_width = cfg.getDiameter() * cfg.getSquareWidthRatio();
-    int inscribed_square_width_i = static_cast<int>(round(inscribed_square_width));
+    double block_width = cfg.getDiameter() * cfg.getSquareWidthRatio();
+    int block_width_i = static_cast<int>(round(block_width));
 
     MicroImageRanges mis = MicroImageRanges::fromConfig(cfg);
 
-    dst.create(mis.getNumY() * inscribed_square_width_i, mis.getNumX() * inscribed_square_width_i, src.type());
+    dst.create(mis.getNumY() * block_width_i, mis.getNumX() * block_width_i, src.type());
     cv::Mat src_roi_image, dst_roi_image;
 
     for (const auto& mi : mis) {
-        getRoiImageByCenter(src, src_roi_image, mi.getCenter(), inscribed_square_width);
-        getRoiImageByLeftupCorner(dst, dst_roi_image, mi.getIndex() * inscribed_square_width_i, inscribed_square_width);
+        getRoiImageByCenter(src, src_roi_image, mi.getCenter(), block_width);
+        getRoiImageByLeftupCorner(dst, dst_roi_image, mi.getIndex() * block_width_i, block_width);
         src_roi_image.copyTo(dst_roi_image);
     }
 }
