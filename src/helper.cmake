4b2fda0ba2f5a9589e0b67047843431302883caf
Starry-OvO <starry.qvq@gmail.com>
2023-09-08 21:12:46 +0800

    refactor: CMake fn `lvc_app_linkage`

--- src/helper.cmake
+++ src/helper.cmake
@@ -1,5 +1,8 @@
-function(lvc_app_copy_dynlib tg)
+function(lvc_app_linkage app lib)
+    target_link_libraries(${app} PUBLIC lib)
+    target_compile_features(${app} PRIVATE cxx_std_20)
+
     if (BUILD_SHARED_LIBS)
-        add_custom_command(TARGET ${tg} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:libLVC_preprocess> $<TARGET_FILE_DIR:${tg}>)
+        add_custom_command(TARGET ${app} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${lib}> $<TARGET_FILE_DIR:${app}>)
     endif ()
 endfunction()
