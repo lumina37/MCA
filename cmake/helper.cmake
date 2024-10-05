4cb11b29e5e584342508857542d7c79848a75b42
lumina37 <starry.qvq@gmail.com>
2024-10-05 23:39:20 +0800

    chore: update TLCT version to 0.14.4

--- cmake/helper.cmake
+++ cmake/helper.cmake
@@ -1,7 +1,7 @@
 function(copy_dlls_if_needed name)
     if (WIN32)
         add_custom_command(TARGET ${name} POST_BUILD COMMAND ${CMAKE_COMMAND} -E
-                copy_if_different $<TARGET_RUNTIME_DLLS:${name}> $<TARGET_FILE_DIR:${name}> COMMAND_EXPAND_LISTS)
+                copy_if_different $<TARGET_RUNTIME_DLLS:${name}> $<TARGET_FILE:${name}> $<TARGET_FILE_DIR:${name}> COMMAND_EXPAND_LISTS)
     endif ()
 endfunction()
 
@@ -14,8 +14,8 @@ if (GIT_FOUND)
             OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 endif ()
-if (NOT TLCT_GIT_TAG)
-    set(TLCT_GIT_TAG "unknown")
+if (NOT MCA_GIT_TAG)
+    set(MCA_GIT_TAG "unknown")
 endif ()
 
 set(MCA_COMPILE_INFO "<tag:${MCA_GIT_TAG}> by [${CMAKE_CXX_COMPILER_ID}-${CMAKE_CXX_COMPILER_VERSION} (${CMAKE_SYSTEM_PROCESSOR})]")
